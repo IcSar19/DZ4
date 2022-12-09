c0178f48d4081446ea0034ef9cd6bec00ae9da02
Daniel <daniel.bozhko1903@mail.ru>
2022-12-09 18:49:03 +0700

    Result

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int b = int.Parse(Console.ReadLine());
+Method1(a, b);
+
+void Method1(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine($"{a} в степени {b} = {result}");
+}
--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿Console.Clear();
+
+Console.Write("Введите любое число ");
+int number = int.Parse(Console.ReadLine());
+int length = NumberLength(number);
+
+SumNumbers(number, length);
+
+
+int NumberLength(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int length)
+{
+    int sum = 0;
+    for (int i = 1; i <= length; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"сумма цифр {sum}");
+}
